a6e042c94e0e994622036e4728be61fd565b7e17
Utsav Gupta <utsavgupta309@gmail.com>
2026-07-12 13:41:28 +0530

    push

---
+++ JSX/JSX_components/src/About.jsx
@@ -0,0 +1,10 @@
+let About = ({width, children}) => {
+  console.log(width);
+  return
+  <nav>
+    <h1>Hey i am about</h1>
+    {children}
+  </nav>
+  
+};
+export default About;
---
+++ JSX/JSX_components/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import About from './About';
+let App = () => {
+  // let ui = React.createElement("h1", {}, [
+  //   React.createElement("h1", {}, "hello"),
+  //   React.createElement("h2", {}, "bye"),
+  //   React.createElement("h3", {}, "No"),
+  // ]); 
+  return (
+     <div>
+        <h1>hello</h1>
+        <h2>bye</h2>
+        <h3>niklllllllll</h3>
+     <About name = "Raj" width  = {90}/>
+      </div>
+  );
+};
+
+export default App;
